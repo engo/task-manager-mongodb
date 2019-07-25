729daa7c716b2a8aa38248ea34e23756c439fc5b
Wilson <wilson@Joannas-iMac.local>
2019-07-25 22:38:34 +1000

    Add middleware

--- src/index.js
+++ src/index.js
@@ -8,10 +8,13 @@ const taskRouter  = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use((req, res, next) => {
+    res.status(503).send('Site is down');
+})
+
 app.use(express.json())
 app.use(userRouter);
 app.use(taskRouter);
-
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,10 +4,11 @@ const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
+    const token = await user.generateAuthToken()
 
     try {
-        await user.save()
-        res.status(201).send(user)
+        await user.save({user, token})
+        res.status(201).send({user, token})
     } catch (e) {
         res.status(400).send(e)
     }
