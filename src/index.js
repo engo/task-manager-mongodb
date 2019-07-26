f554e1826b69a41f3161c8abbd629cf9c6aecd39
Wilson <wilson@Joannas-iMac.local>
2019-07-26 21:59:57 +1000

    Add auth

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ const taskRouter  = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use((req, res, next) => {
-    res.status(503).send('Site is down');
-})
+
 
 app.use(express.json())
 app.use(userRouter);
